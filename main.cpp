59ef06c00a449811b2a5bcc99a4be4a10e7e39f3
Bigodeiro <enzobigber@gmail.com>
2023-01-18 17:47:35 -0300

    Simulaçao de órbita

--- main.cpp
+++ main.cpp
@@ -3,52 +3,96 @@
 #include <typeinfo> //* bib para descobrir o tipo de certas variaveis, usa "typeid(<variavel>).name" para conseguir o nome
 #include <cmath>
 #include <math.h>
-#define WIDTH 800
-#define HEIGTH 600
+#define WIDTH 1200
+#define HEIGTH 800
 
+class cord 
+{
+  public:
+
+    float x;
+    float y;
+};
+
+class circleObj 
+{
+  public:
+
+    int mass;
+    float radius;
+
+    cord pos;
+    cord velocity;
+
+    sf::CircleShape sfCircle;
+    sf::Color color;
+
+
+    void applyChanges()
+    {
+        sfCircle.setFillColor(color);
+        sfCircle.setRadius(radius);
+
+        pos.x += velocity.x;
+        pos.y += velocity.y;
+
+        sfCircle.setPosition(pos.x - radius, pos.y - radius);
+    };
+
+
+    circleObj(float xPos, float yPos, float cRadius, int cMass, sf::Color cColor)
+    {
+        radius = cRadius;
+        mass = cMass;
+        color = cColor;
+
+        velocity.x = 0;
+        velocity.y = 0;
+
+        pos.x = xPos;
+        pos.y = yPos;
+
+        sfCircle = sf::CircleShape(cRadius);
+        sfCircle.setFillColor(color);
+    };
+};
+
+circleObj grav(circleObj corpoFixo, circleObj corpoMovel)
+{
+    float distanciaX = corpoFixo.pos.x - corpoMovel.pos.x;
+    float distanciaY = corpoFixo.pos.y - corpoMovel.pos.y;
+
+    float distanciaEntreCentros = sqrt(pow(distanciaX, 2)  + pow(distanciaY, 2));
+
+    float forcaAplicada = ((corpoFixo.mass * corpoMovel.mass) / (pow(distanciaEntreCentros, 2))/10);
+
+    float distanciaTotal = abs(distanciaX) + abs(distanciaY);
+
+    float forcaX = (distanciaX / distanciaTotal) * forcaAplicada;
+    float forcaY = (distanciaY / distanciaTotal) * forcaAplicada;
+
+    corpoMovel.velocity.x += forcaX;
+    corpoMovel.velocity.y += forcaY;
+
+    return corpoMovel;
+}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGTH), "SFML!", sf::Style::Close | sf::Style::Titlebar);
 
-    sf::CircleShape player(50.0f);
-    player.setFillColor(sf::Color::Cyan);
+    circleObj vermelho(WIDTH/2, HEIGTH/2, 10, 100, sf::Color::Red);
 
-    sf::CircleShape circuloParado(50.0f);
-    circuloParado.setFillColor(sf::Color::Red);
-    circuloParado.setPosition(sf::Vector2f(WIDTH/2 - 50, HEIGTH/2 - 50));
+    circleObj verde(400, 600, 25, 1, sf::Color::Green);
+    verde.velocity.x = 0.15f;
+
+
 
     while (window.isOpen())//! cada iteração aqui é um frame
     {
         sf::Event evnt;
-
-        //* move o player
-        player.setPosition(sf::Vector2f(evnt.mouseMove.x - 50, evnt.mouseMove.y - 50));
-
-
-        //? Calcula se houve colisão
-        bool colisao = false;
-
-        int distanciaX = circuloParado.getPosition().x - player.getPosition().x;
-        int distanciaY = circuloParado.getPosition().y - player.getPosition().y;
-
-        int distanciaEntreCentros = sqrt(pow(distanciaX, 2)  + pow(distanciaY, 2));
-
-        
-
-        if (distanciaEntreCentros < 100)
-        {
-            colisao = true;
-        }else
-        {
-            colisao = false;
-        }
-        
-        
-
         while (window.pollEvent(evnt))
         {
-            if (colisao) printf("As esferas estao colidindo\n");
             switch (evnt.type)
             {
             case sf::Event::Closed:
@@ -56,23 +100,22 @@ int main()
                 window.close();
 
                 break;
-
-            // case sf::Event::TextEntered:
-                
-            //     char input = evnt.text.unicode;
-
-            //     //? sim, é possivel comparar char com int
-            //     if (input < 128)  printf("%c", input);
-                
-            //     break;
             }
         }
+
     window.clear();
 
+    //!codigo a ser executado todo frame
+
+    verde = grav(vermelho, verde);
 
     //? renderiza algo para a tela, modificando-a
-    window.draw(player);
-    window.draw(circuloParado);
+    vermelho.applyChanges();
+    window.draw(vermelho.sfCircle);
+
+    verde.applyChanges();
+    window.draw(verde.sfCircle);
+
 
     //? recarrega a tela, fazendo com que as mudanças sejam efetivadas
     window.display();
